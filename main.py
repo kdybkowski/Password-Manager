a8108c7fbd93f3532d3a39c8443315393ef92b28
Karol Dybkowski <k.dybkowski@gmail.com>
2024-11-24 20:14:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -14,9 +14,9 @@ BLUE = '#0B60B0'
 FONT_1 = ('Arial', 12, 'bold')
 FONT_2 = ('Arial', 12)
 
+# Get absolute path to resource
 
 def resource_path(relative_path):
-    """ Get absolute path to resource, works for dev and for PyInstaller """
     base_path = getattr(sys, '_MEIPASS', path.dirname(path.abspath(__file__)))
     return path.join(base_path, relative_path)
 
